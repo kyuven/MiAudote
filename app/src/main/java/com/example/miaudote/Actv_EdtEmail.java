4997e55cb94f397294d5666e8d266416582892df
kyuven <101908843+kyuven@users.noreply.github.com>
2024-06-29 08:23:42 -0300

    - Adicionado atividades de Edição de Dados Pessoais, de animal, E-Mail e Senha; - Feito o Design da edição de e-mail, senha e edição de dados do animal; - Fragment de perfil layout arrumado (início do layout com dialog custom pra tela de edição de perfil; - Arrumado alguns detalhes de design responsivo.

---
+++ app/src/main/java/com/example/miaudote/Actv_EdtEmail.java
@@ -0,0 +1,20 @@
+package com.example.miaudote;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Actv_EdtEmail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_actv_edt_email);
+
+    }
+}
--- app/src/main/java/com/example/miaudote/Perfil_Fragment.java
+++ app/src/main/java/com/example/miaudote/Perfil_Fragment.java
@@ -17,13 +17,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 
 public class Perfil_Fragment extends Fragment {
 
     AppCompatButton btnMudarPerfil, btnTxtLogOut, btnCancelLogout, btnConfirmLogout;
-    AppCompatImageButton btnMudarSenha, btnTermos, btnSobreNos, btnIconLogOut;
+    ImageButton btnBack;
+    AppCompatImageButton btnTermos, btnSobreNos, btnIconLogOut, btnEdtDados, btnEdtEmail, btnEdtSenha;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -34,24 +36,25 @@ public class Perfil_Fragment extends Fragment {
         btnMudarPerfil = view.findViewById(R.id.btnPerfil_mudarPerfil);
         btnTxtLogOut = view.findViewById(R.id.btnPerfil_txtLogOut);
 
-        btnMudarSenha = view.findViewById(R.id.btnPerfil_mudarSenha);
         btnTermos = view.findViewById(R.id.btnPerfil_termos);
         btnSobreNos = view.findViewById(R.id.btnPerfil_sobreNos);
         btnIconLogOut = view.findViewById(R.id.btnPerfil_iconLogOut);
 
         btnIconLogOut.setOnClickListener(v -> {
-            View alertCustomDialog = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_custom, null);
+            View alertCustomDialog = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_custom, null); // passa para a variável o layout
             AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
 
-            alertDialog.setView(alertCustomDialog);
+            alertDialog.setView(alertCustomDialog); // Aciona, no fragment, o layout de logout
+            // pega os botões da "caixa de dialogo"
             btnCancelLogout = alertCustomDialog.findViewById(R.id.btnDialogCustom_nao);
             btnConfirmLogout = alertCustomDialog.findViewById(R.id.btnDialogCustom_sim);
 
+            // Agora, de fato, abre a caixa de diálogo, colocando o fundo como transparente (aparece a atividade)
             final AlertDialog dialog = alertDialog.create();
             dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
             dialog.show();
 
-            btnCancelLogout.setOnClickListener(v1 -> dialog.cancel());
+            btnCancelLogout.setOnClickListener(v1 -> dialog.cancel()); // Fecha a caixa de diálogo caso o botão seja pressionado
             btnConfirmLogout.setOnClickListener(v12 -> {
                 Intent intent = new Intent(getActivity(), LoginIn_Activity.class);
                 startActivity(intent);
@@ -59,8 +62,23 @@ public class Perfil_Fragment extends Fragment {
         });
 
         btnMudarPerfil.setOnClickListener(v -> {
-            Intent intent = new Intent(getActivity(), Change_Perfil.class);
-            startActivity(intent);
+            View alrtDialog = LayoutInflater.from(getActivity()).inflate(R.layout.change_profile_options, null);
+            AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+
+            btnBack = alrtDialog.findViewById(R.id.fab_backEdtDados);
+            btnEdtDados = alrtDialog.findViewById(R.id.btnEdtDados);
+            btnEdtEmail = alrtDialog.findViewById(R.id.btnEdtEmail);
+            btnEdtSenha = alrtDialog.findViewById(R.id.btnEdtSenha);
+
+            alert.setView(alrtDialog);
+            final  AlertDialog dialog = alert.create();
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+            dialog.show();
+
+            btnBack.setOnClickListener(v13 -> dialog.cancel()); // Cancela a caixa de diálogo
+            btnEdtDados.setOnClickListener(v14 -> startActivity(new Intent(getActivity(), Actv_EdtDados.class))); // Abre a atividade para editar dados
+            btnEdtEmail.setOnClickListener(v15 -> startActivity(new Intent(getActivity(), Actv_EdtEmail.class))); // Abre a atividade para editar email
+            btnEdtSenha.setOnClickListener(v16 -> startActivity(new Intent(getActivity(), Actv_EdtSenha.class))); // Abre a atividade para editar senha
         });
 
         return view;
