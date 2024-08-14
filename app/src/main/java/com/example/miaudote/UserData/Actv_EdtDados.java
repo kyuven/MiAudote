fb74dcb230ea2978cfb0f83aa1503f2f0aab693b
Leonardo <leo11cordeiro@gmail.com>
2024-08-14 17:44:04 -0300

    Update nome Adt_EditDados
    falta parte da foto e terminar os termos de uso

--- app/src/main/java/com/example/miaudote/UserData/Actv_EdtDados.java
+++ app/src/main/java/com/example/miaudote/UserData/Actv_EdtDados.java
@@ -1,10 +1,20 @@
 package com.example.miaudote.UserData;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.AppCompatImageButton;
@@ -13,21 +23,29 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.miaudote.Fragments.Perfil_Fragment;
+import com.example.miaudote.Models.UserModel;
+import com.example.miaudote.ONGInfo.ONG_Register_Contact;
+import com.example.miaudote.ONGInfo.ONG_Register_General;
 import com.example.miaudote.R;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class Actv_EdtDados extends AppCompatActivity {
 
     FirebaseAuth mAuth;
     FirebaseUser firebaseUser;
     DatabaseReference reference;
-    ImageView edt_fotoPerfil;
-    TextInputEditText edt_novoNome;
+    StorageReference storageReference;
     AppCompatButton btnUploadFoto, btnAtualizarDados;
+    TextInputEditText edtNovoNome;
+    Uri uriImage;
+    String imgUserStr, nomeUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +55,9 @@ public class Actv_EdtDados extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         firebaseUser = mAuth.getCurrentUser();
         reference = FirebaseDatabase.getInstance().getReference("usuarios");
+        // storageReference = FirebaseStorage.getInstance().getReference("Imagens Usuarios");
 
-        TextInputEditText edtNovoNome = findViewById(R.id.edt_novoNome);
+        edtNovoNome = findViewById(R.id.edt_novoNome);
         ImageView fotoPerfil = findViewById(R.id.edt_fotoPerfil);
 
         btnUploadFoto = findViewById(R.id.btnAttFotoPerfil);
@@ -51,5 +70,46 @@ public class Actv_EdtDados extends AppCompatActivity {
             finish();
         });
 
+        // Update foto de perfil do usuário
+        ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult o) {
+                        if(o.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = o.getData();
+                            uriImage = data.getData();
+                            fotoPerfil.setImageURI(uriImage);
+                        } else {
+                            Toast.makeText(Actv_EdtDados.this, "Nenhuma imagem selecionada.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+        btnUploadFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoPicker = new Intent(Intent.ACTION_PICK);
+                photoPicker.setType("image/*");
+                activityResultLauncher.launch(photoPicker);
+            }
+        });
+
+        // Update dados (foto de perfil e nome)
+        btnAtualizarDados.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateData(firebaseUser);
+            }
+        });
+    }
+
+
+
+    private void updateData(FirebaseUser firebaseUser){
+        nomeUser = edtNovoNome.getText().toString().trim();
+        String userID = firebaseUser.getUid();
+        reference.child(userID).child("nome").setValue(nomeUser);
+        finish();
     }
 }
