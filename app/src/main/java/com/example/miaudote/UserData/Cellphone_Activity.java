b14532d2a9cd0767a01fac1f25e8fe86c598bfc2
leoleozao <eduardalostodalostodecarvalho@gmail.com>
2024-08-21 18:05:45 -0300

    Verificação OTP - concluído
    AAAAAAHHHHH MULUQUEEEEEE
    agora é só FIFA com meus manos.
    OTP feito com sucesso, tem que só arrumar o finish, etc

--- app/src/main/java/com/example/miaudote/UserData/Cellphone_Activity.java
+++ app/src/main/java/com/example/miaudote/UserData/Cellphone_Activity.java
@@ -5,16 +5,25 @@ import android.os.Bundle;
 import android.telephony.PhoneNumberUtils;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.miaudote.Models.UserModel;
 import com.example.miaudote.R;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 public class Cellphone_Activity extends AppCompatActivity {
 
@@ -40,17 +49,22 @@ public class Cellphone_Activity extends AppCompatActivity {
 
         // Telefone
         String userID = firebaseUser.getUid();
-
         edtTefone = findViewById(R.id.edtTelefone);
-        strTelefone = PhoneNumberUtils.formatNumber(edtTefone.getText().toString());
 
         // Envia para a página de confirmação de cadastro (código OTP)
         btnConfirmarCad = findViewById(R.id.fab_next_tel);
         btnConfirmarCad.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                reference.child(userID).child("telefone").setValue(strTelefone);
-                startActivity(new Intent(Cellphone_Activity.this, Confirmar_Login.class));
+                if (edtTefone.getText().toString().trim().isEmpty()){
+                    Toast.makeText(Cellphone_Activity.this, "Por favor, digite seu telefone", Toast.LENGTH_SHORT).show();
+                }
+
+                strTelefone = edtTefone.getText().toString();
+                Intent intent = new Intent(Cellphone_Activity.this, Confirmar_Login.class);
+                intent.putExtra("telefone", strTelefone);
+                startActivity(intent);
+
 
             }
         });
--- app/src/main/java/com/example/miaudote/UserData/Confirmar_Login.java
+++ app/src/main/java/com/example/miaudote/UserData/Confirmar_Login.java
@@ -1,5 +1,6 @@
 package com.example.miaudote.UserData;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -15,201 +16,133 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.miaudote.Fragments.Main_Page;
 import com.example.miaudote.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
 public class Confirmar_Login extends AppCompatActivity {
 
-    EditText inputCodigo01, inputCodigo02, inputCodigo03, inputCodigo04, inputCodigo05, inputCodigo06;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+    EditText inputCodigo;
     Button btn_reenviar;
+    ImageButton btn_finalcad;
+    String StrTelefone, telefone, verificationCode;
 
-    private boolean reenviarEnabled = false;
-
-    private int reenviarTime = 60;
-
-    private int selectedInputCodigo = 0;
+    PhoneAuthProvider.ForceResendingToken resendingToken;
+    Long timeoutSeconds = 60L;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmar_login);
 
-        inputCodigo01 = findViewById(R.id.inputCodigo01);
-        inputCodigo02 = findViewById(R.id.inputCodigo02);
-        inputCodigo03 = findViewById(R.id.inputCodigo03);
-        inputCodigo04 = findViewById(R.id.inputCodigo04);
-        inputCodigo05 = findViewById(R.id.inputCodigo05);
-        inputCodigo06 = findViewById(R.id.inputCodigo06);
+        inputCodigo = findViewById(R.id.inputCodigo);
+        btn_finalcad = findViewById(R.id.btn_final_cad);
         btn_reenviar = findViewById(R.id.btn_reenviar);
 
-        final String telefone = getIntent().getStringExtra("telefone");
+        StrTelefone = getIntent().getExtras().getString("telefone");
+        telefone = "+55" + StrTelefone;
 
-        inputCodigo01.addTextChangedListener(textWatcher);
-        inputCodigo02.addTextChangedListener(textWatcher);
-        inputCodigo03.addTextChangedListener(textWatcher);
-        inputCodigo04.addTextChangedListener(textWatcher);
-        inputCodigo05.addTextChangedListener(textWatcher);
-        inputCodigo06.addTextChangedListener(textWatcher);
+        sendOTP(telefone, false);
 
-        // Abrir o teclado para digitar no código 01
-        showKeybord(inputCodigo01);
-
-        // Contagem para reenviar o código
-        startCountDownTime();
+        btn_finalcad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String enteredOTP = inputCodigo.getText().toString();
+                PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationCode, enteredOTP);
+                signIn(credential);
+            }
+        });
 
         btn_reenviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                if(reenviarEnabled){
-
-                    // Reiniciar a contagem para reenvio do código
-                    startCountDownTime();
-                }
-
+                sendOTP(telefone, true);
             }
         });
+    }
 
+    void sendOTP(String telefone, boolean isResend){
+        startResendTimer();
+        PhoneAuthOptions.Builder builder =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(telefone)
+                        .setTimeout(timeoutSeconds, TimeUnit.SECONDS)
+                        .setActivity(this)
+                        .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                            @Override
+                            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
 
+                            }
 
-        // Confirma o cadastro do usuário e envia para a página principal
-        ImageButton btnCadastrar = (ImageButton) findViewById(R.id.btn_final_cad);
-        btnCadastrar.setOnClickListener(v -> {
+                            @Override
+                            public void onVerificationFailed(@NonNull FirebaseException e) {
+                                Toast.makeText(Confirmar_Login.this, "Verificação OTP falhou!", Toast.LENGTH_SHORT).show();
+                            }
 
-            final String generateOtp = inputCodigo01.getText().toString()+inputCodigo02.getText().toString()+inputCodigo03.getText().toString()+
-                    inputCodigo04.getText().toString()+inputCodigo05.getText().toString()+inputCodigo06.getText().toString();
+                            @Override
+                            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                                super.onCodeSent(s, forceResendingToken);
+                                verificationCode = s;
+                                resendingToken = forceResendingToken;
+                                Toast.makeText(Confirmar_Login.this, "Verificação OTP foi realizada com sucesso!", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+        if(isResend){
+            PhoneAuthProvider.verifyPhoneNumber(builder.setForceResendingToken(resendingToken).build());
+        }else {
+            PhoneAuthProvider.verifyPhoneNumber(builder.build());
+        }
+    }
 
-            if (generateOtp.length() == 4){
-
+    void signIn(PhoneAuthCredential phoneAuthCredential){
+        mAuth.signInWithCredential(phoneAuthCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Intent intent = new Intent(Confirmar_Login.this, Main_Page.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(Confirmar_Login.this, "Verificação OTP falhou!", Toast.LENGTH_SHORT).show();
+                }
             }
-
-            Intent intent = new Intent(Confirmar_Login.this, Main_Page.class);
-            overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
-            startActivity(intent);
-            finish();
         });
     }
 
-
-    private void showKeybord(EditText inputCodigo){
-
-        inputCodigo.requestFocus();
-
-        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        inputMethodManager.showSoftInput(inputCodigo, InputMethodManager.SHOW_IMPLICIT);
-    }
-
-    private void startCountDownTime(){
-        reenviarEnabled = false;
-        btn_reenviar.setTextColor(Color.parseColor("#99000000"));
-
-        new CountDownTimer(reenviarTime * 1000, 1000){
-
+    void startResendTimer(){
+        btn_reenviar.setEnabled(false);
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
             @Override
-            public void onTick(long millisUntilFinished) {
-                btn_reenviar.setText("Reenviar Código ("+(millisUntilFinished /1000) +")");
+            public void run() {
+                timeoutSeconds--;
+                btn_reenviar.setText("Reenviar OTP em "+timeoutSeconds +" segundos");
+                if (timeoutSeconds<=0){
+                    timeoutSeconds = 60L;
+                    timer.cancel();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            btn_reenviar.setEnabled(true);
+                        }
+                    });
+                }
             }
-
-            @Override
-            public void onFinish() {
-                reenviarEnabled = true;
-                btn_reenviar.setText("Reenviar código");
-                btn_reenviar.setTextColor(getResources().getColor(R.color.blue_link));
-            }
-        }.start();
+        }, 0, 1000);
     }
 
-    private final TextWatcher textWatcher = new TextWatcher() {
-        @Override
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-        }
-
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-        }
-
-        @Override
-        public void afterTextChanged(Editable s) {
-
-            if(s.length() > 0){
-                if(selectedInputCodigo == 0){
-
-                    selectedInputCodigo = 1;
-                    showKeybord(inputCodigo02);
-                }
-
-                else if (selectedInputCodigo == 1){
-
-                    selectedInputCodigo = 2;
-                    showKeybord(inputCodigo03);
-                }
-
-                else if (selectedInputCodigo == 2){
-
-                    selectedInputCodigo = 3;
-                    showKeybord(inputCodigo04);
-                }
-
-                else if (selectedInputCodigo == 3){
-
-                    selectedInputCodigo = 4;
-                    showKeybord(inputCodigo05);
-                }
-
-                else if (selectedInputCodigo == 4){
-
-                    selectedInputCodigo = 5;
-                    showKeybord(inputCodigo06);
-                }
-            }
-        }
-    };
-
-    public boolean onKeyUp(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_DEL){
-
-            if (selectedInputCodigo == 5){
-                selectedInputCodigo = 4;
-
-                showKeybord(inputCodigo05);
-            }
-
-            else if (selectedInputCodigo == 4) {
-                selectedInputCodigo = 3;
-
-                showKeybord(inputCodigo04);
-            }
-
-            else if(selectedInputCodigo == 3 ){
-                selectedInputCodigo = 2;
-
-                showKeybord(inputCodigo03);
-            }
-
-            else if (selectedInputCodigo == 2) {
-                selectedInputCodigo = 1;
-
-                showKeybord(inputCodigo02);
-            }
-
-            else if (selectedInputCodigo == 1) {
-                 selectedInputCodigo = 0;
-
-                 showKeybord(inputCodigo01);
-            }
-
-            return true;
-
-        } else {
-            return super.onKeyUp(keyCode, event);
-        }
-    }
-
-    public void onBackPressed(){
-        super.onBackPressed();
-        overridePendingTransition(R.anim.slide_from_left, R.anim.slide_to_right);
-    }
 }
